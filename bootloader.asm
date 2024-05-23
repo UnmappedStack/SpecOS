c35f76bf5faddc9339ca7bbff827c910f87e63ca
jakeSteinburger <163998065+jakeSteinburger@users.noreply.github.com>
2024-05-24 08:06:08 +1000

    Bug fixes (loaded more sectors)

--- bootloader.asm
+++ bootloader.asm
@@ -27,7 +27,7 @@ load_kernel:
     call print_nl
 
     mov bx, KERNEL_OFFSET ; Read from disk and store in 0x1000
-    mov dh, 2
+    mov dh, 13
     mov dl, [BOOT_DRIVE]
     call disk_load
     ret
