b80fac2e7e77d57d86f443f8ebacfb178abea133
Jake Steinburger <jakes@archlinux.localdomain>
2024-06-12 20:11:52 +0000

    Minor mistakes fixed (not a bug)

--- drivers/rtc.c
+++ drivers/rtc.c
@@ -2,27 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "rtc.h"
-
-static inline uint8_t inb(uint16_t port)
-{
-    uint8_t ret;
-    __asm__ volatile ( "inb %w1, %b0"
-                   : "=a"(ret)
-                   : "Nd"(port)
-                   : "memory");
-    return ret;
-}
-
-static inline void outb(uint16_t port, uint8_t val)
-{
-    __asm__ volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
-    /* There's an outb %al, $imm8 encoding, for compile-time constant port numbers that fit in 8b. (N constraint).
-     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
-     * The  outb  %al, %dx  encoding is the only option for all other cases.
-     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
-}
-
-
+#include "../utils/inx.h"
 
 enum {
       cmos_address = 0x70,
